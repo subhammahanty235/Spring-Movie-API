e56c7b9c38d7c704844dda55f9253f93b15c01f6
subhammahanty235 <subhammahanty235@gmail.com>
2023-01-26 23:48:43 +0530

    Initial

---
+++ src/main/java/com/submoviewdata/movies/MoviesApiApplication.java
@@ -0,0 +1,18 @@
+package com.submoviewdata.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+//@RestController
+public class MoviesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApiApplication.class, args);
+	}
+
+
+
+
+}
---
+++ src/test/java/com/submoviewdata/movies/MoviesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.submoviewdata.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
