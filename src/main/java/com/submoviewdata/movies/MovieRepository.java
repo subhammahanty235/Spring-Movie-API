1618726de3b7f45f034209103b5c55a504869870
subhammahanty235 <subhammahanty235@gmail.com>
2023-01-28 23:34:16 +0530

    Movie APi

---
+++ src/main/java/com/submoviewdata/movies/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.submoviewdata.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie , ObjectId> {
+
+}
